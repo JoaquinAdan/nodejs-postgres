8656ca8b73bccc43f3897a96e99cd3fc4d4343ae
JoaquinAdan <rodriguezjdev@gmail.com>
2025-01-19 23:00:40 +0100

    first commit

--- services/product.service.js
+++ services/product.service.js
@@ -1,15 +1,16 @@
-const { faker } = require('@faker-js/faker');
-const boom = require('@hapi/boom');
+const { faker } = require('@faker-js/faker')
+const boom = require('@hapi/boom')
+
+const sequelize = require('../libs/sequelize')
 
 class ProductsService {
-
-  constructor(){
-    this.products = [];
-    this.generate();
+  constructor() {
+    this.products = []
+    this.generate()
   }
 
   generate() {
-    const limit = 100;
+    const limit = 100
     for (let index = 0; index < limit; index++) {
       this.products.push({
         id: faker.datatype.uuid(),
@@ -17,56 +18,57 @@ class ProductsService {
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.imageUrl(),
         isBlock: faker.datatype.boolean(),
-      });
+      })
     }
   }
 
   async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
-      ...data
+      ...data,
     }
-    this.products.push(newProduct);
-    return newProduct;
+    this.products.push(newProduct)
+    return newProduct
   }
 
-  find() {
-    return this.products;
+  async find() {
+    const query = 'SELECT * FROM tasks'
+    const [data] = await sequelize.query(query)
+    return data
   }
 
   async findOne(id) {
-    const product = this.products.find(item => item.id === id);
+    const product = this.products.find((item) => item.id === id)
     if (!product) {
-      throw boom.notFound('product not found');
+      throw boom.notFound('product not found')
     }
     if (product.isBlock) {
-      throw boom.conflict('product is block');
+      throw boom.conflict('product is block')
     }
-    return product;
+    return product
   }
 
   async update(id, changes) {
-    const index = this.products.findIndex(item => item.id === id);
+    const index = this.products.findIndex((item) => item.id === id)
     if (index === -1) {
-      throw boom.notFound('product not found');
+      throw boom.notFound('product not found')
     }
-    const product = this.products[index];
+    const product = this.products[index]
     this.products[index] = {
       ...product,
-      ...changes
-    };
-    return this.products[index];
+      ...changes,
+    }
+    return this.products[index]
   }
 
   async delete(id) {
-    const index = this.products.findIndex(item => item.id === id);
+    const index = this.products.findIndex((item) => item.id === id)
     if (index === -1) {
-      throw boom.notFound('product not found');
+      throw boom.notFound('product not found')
     }
-    this.products.splice(index, 1);
-    return { id };
+    this.products.splice(index, 1)
+    return { id }
   }
-
 }
 
-module.exports = ProductsService;
+module.exports = ProductsService
--- services/user.service.js
+++ services/user.service.js
@@ -1,30 +1,42 @@
-const boom = require('@hapi/boom');
+// const boom = require('@hapi/boom')
+
+// const getConnection = require('../libs/postgres')
+// const pool = require('../libs/postgres.pool')
+const { models } = require('./../libs/sequelize')
 
 class UserService {
-  constructor() {}
+  constructor() {
+    // this.pool = pool
+    // this.pool.on('error', (err) => console.error('Unexpected error on idle client', err))
+  }
 
   async create(data) {
-    return data;
+    return data
   }
 
   async find() {
-    return [];
+    // const client = await getConnection()
+    // const query = 'SELECT * FROM tasks'
+    // const rta = await this.pool.query(query)
+    // return rta.rows
+    const rta = await models.User.findAll()
+    return rta
   }
 
   async findOne(id) {
-    return { id };
+    return { id }
   }
 
   async update(id, changes) {
     return {
       id,
       changes,
-    };
+    }
   }
 
   async delete(id) {
-    return { id };
+    return { id }
   }
 }
 
-module.exports = UserService;
+module.exports = UserService

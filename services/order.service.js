788bb5d5a0936bd9200e428d6e2b94e9fb536518
JoaquinAdan <rodriguezjdev@gmail.com>
2025-01-25 23:42:33 +0100

    :construction: Add order model, migration, service, and routes for order management

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,37 @@
-const boom = require('@hapi/boom');
+const boom = require('@hapi/boom')
+
+const { models } = require('./../libs/sequelize')
 
 class OrderService {
-
-  constructor(){
-  }
+  constructor() {}
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data)
+    return newOrder
   }
 
   async find() {
-    return [];
+    const orders = await models.Product.findAll()
+    return orders
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id)
+    if (!order) {
+      throw boom.notFound('Order not found')
+    }
+    return order
   }
 
   async update(id, changes) {
     return {
       id,
       changes,
-    };
+    }
   }
 
   async delete(id) {
-    return { id };
+    return { id }
   }
-
 }
 
-module.exports = OrderService;
+module.exports = OrderService
